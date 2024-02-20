9e96146217ef4b8ed840e9dfde28345337e15a26
Jarosław K <jarasss1@wp.pl>
2024-02-20 22:24:51 +0100

    add plan selection

--- form.js/plan.js
+++ form.js/plan.js
@@ -1,13 +1,18 @@
-const yourPlan = [...document.querySelectorAll('.selectPlate__plan')];
+const yourPlan = [...document.querySelectorAll('div.selectPlate__plan')];
 const toggle = document.querySelector('.selectPlate__toggle');
+
 // wybór planu//
-yourPlan.forEach((e) => {
-  e.addEventListener('click', function (e) {
+// Dodanie klasy wybrenemu elementowi.
+yourPlan.forEach((click) => {
+  click.addEventListener('click', () => {
     yourPlan.forEach((clean) => clean.classList.remove('selectPlate__active'));
-    e.target.classList.add('selectPlate__active');
+    click.classList.add('selectPlate__active');
   });
 });
-
+//Przełącznik monthly / yearly
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('selectPlate__toggle--active');
+
+  toggle.childNodes[5].classList.toggle('toggle__option--active');
+  toggle.childNodes[1].classList.toggle('toggle__option--active');
 });
